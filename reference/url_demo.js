90a5814a5818d8f26d2f05a4cefa49c1bbede5ae
Shadman-Ahmed-Chowdhury <shadmanchowdhury87@gmail.com>
2020-12-06 08:23:46 +0600

    url_demo added

---
+++ reference/url_demo.js
@@ -0,0 +1,29 @@
+const url = require("url");
+
+const myUrl = new URL("https://mywebsite.com/hello.html?id=100&status=active");
+
+//Serialized URL
+
+console.log(myUrl.href);
+
+//Host(root Domain)
+console.log(myUrl.host);
+
+// Hostname(does not get port)
+console.log(myUrl.hostname);
+
+//PathName
+console.log(myUrl.pathname);
+
+//Serialized Query
+console.log(myUrl.search);
+
+// Search Params object
+console.log(myUrl.searchParams);
+
+// Add param
+myUrl.searchParams.append("abc", "123");
+console.log(myUrl.searchParams);
+
+//Loop through the params
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`));
