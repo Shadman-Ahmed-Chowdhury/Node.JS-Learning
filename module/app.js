5851a5e661b89f3845f2eef649dcee3a8b640f69
Shadman-Ahmed-Chowdhury <shadmanchowdhury87@gmail.com>
2020-12-15 08:50:59 +0600

    module demo added

---
+++ module/app.js
@@ -0,0 +1,7 @@
+var logger = require("./logger");
+
+//After exporting as a function
+logger.log("From the logger as object");
+
+// After exporting function
+//logger("from the function");
---
+++ module/logger.js
@@ -0,0 +1,9 @@
+function log(message) {
+  console.log(message);
+}
+
+//exporting as object
+module.exports.log = log;
+
+//Exporting as function
+//module.exports = log;
